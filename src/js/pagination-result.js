e8e79538ee61ac33ea7ce7400542b25cbfe67e07
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-07-17 20:21:00 +0300

    update pagination

--- src/js/pagination-result.js
+++ src/js/pagination-result.js
@@ -39,35 +39,35 @@ async function loadMoreTrendMovies(currentPage) {
 
 // Пагінація і налаштування для пошуку фільмів 
 // (потрібно налаштований пошук, щоб взяти searchQuery)
-async function loadSearchMovies() {
-  try {
-    const movies = await fetchBySearchMovies(searchQuery, page = 1);
-    console.log(movies);
-    renderCollection(movies);
+// async function loadSearchMovies() {
+//   try {
+//     const movies = await fetchBySearchMovies(searchQuery, page = 1);
+//     console.log(movies);
+//     renderCollection(movies);
 
-    const instance = createPagination();
-    instance.setItemsPerPage(20);
-    instance.setTotalItems(movies.total_results);
-    instance.movePageTo(movies.page);
+//     const instance = createPagination();
+//     instance.setItemsPerPage(20);
+//     instance.setTotalItems(movies.total_results);
+//     instance.movePageTo(movies.page);
 
-    instance.on('afterMove', event => {
-      const currentPage = event.page;
-      window.scrollTo({ top: 240, behavior: 'smooth' });
-      loadMoreSearchMovies(currentPage);
-    });
-  }
-  catch (error) { console.log(error); };
-}
+//     instance.on('afterMove', event => {
+//       const currentPage = event.page;
+//       window.scrollTo({ top: 240, behavior: 'smooth' });
+//       loadMoreSearchMovies(currentPage);
+//     });
+//   }
+//   catch (error) { console.log(error); };
+// }
 
-async function loadMoreSearchMovies(currentPage) {
-  try {
-    const searchMovies = await fetchBySearchMovies(searchQuery, currentPage);
-    clearPreviousResults();
-    renderCollection(searchMovies);
-    console.log(searchMovies);
-  }
-  catch (error) { console.log(error); }
-}
+// async function loadMoreSearchMovies(currentPage) {
+//   try {
+//     const searchMovies = await fetchBySearchMovies(searchQuery, currentPage);
+//     clearPreviousResults();
+//     renderCollection(searchMovies);
+//     console.log(searchMovies);
+//   }
+//   catch (error) { console.log(error); }
+// }
 
 function clearPreviousResults() {
   if (moviesList.hasChildNodes() === true) {
