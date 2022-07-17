ac2e51af39817ab3f583973ef0798f4c7aebf533
Artem Horiuk <62579471+artemhoriuk1994@users.noreply.github.com>
2022-07-17 04:00:30 +0300

    Create spinner

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import '../src/js/pagination-result';
 import '../src/js/to-top-btn';
+<<<<<<< HEAD
 import '../src/js/api-fetch';
+=======
+>>>>>>> 9dff3fb (Create spinner)
 import '../src/js/spiner';
