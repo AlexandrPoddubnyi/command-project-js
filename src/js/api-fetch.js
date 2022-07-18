61f2ae17e4abfa78985153183345651bce74334c
Yurii <ukucher@gmail.com>
2022-07-18 23:46:34 +0300

    1-st version of module is done

--- src/js/api-fetch.js
+++ src/js/api-fetch.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
-import { KEY, DEFAULT_URL, BY_TRENDS, BY_SEARCH, BY_ID, renderPoster } from './api-keys';
+import {
+  KEY,
+  DEFAULT_URL,
+  BY_TRENDS,
+  BY_SEARCH,
+  BY_ID,
+  renderPoster,
+} from './api-keys';
 import { renderTrendCollection, renderOneFilm } from './render-trends';
 export { fetchTrendMovies, fetchBySearchMovies, fetchByID };
 
-
-
-
 // Fetch полной инф-ы по трендам
-async function fetchTrendMovies( page = 1) {
+async function fetchTrendMovies(page = 1) {
   try {
     const { data } = await axios.get(
       `${BY_TRENDS}?api_key=${KEY}&page=${page}`
@@ -18,28 +22,26 @@ async function fetchTrendMovies( page = 1) {
   }
 }
 
-
-  //Fetch by Search
-  async function fetchBySearchMovies(formInput, page = 1) {
-    try {
-      const { data } = await axios.get(
-        `${BY_SEARCH}?api_key=${KEY}&query=${formInput}&page=${page}`
-      );
-      return data;
-    } catch (error) {
-      console.error('ERROR');
-    }
+//Fetch by Search
+async function fetchBySearchMovies(formInput, page = 1) {
+  try {
+    const { data } = await axios.get(
+      `${BY_SEARCH}?api_key=${KEY}&query=${formInput}&page=${page}`
+    );
+    return data;
+  } catch (error) {
+    console.error('ERROR');
   }
+}
 
-  async function fetchByID(id) {
-    try {
-      const { data } = await axios.get(`${BY_ID}${507086}?api_key=${KEY}`);
-      return data;
-    } catch (error) {
-      console.error('ERROR');
-    }
+async function fetchByID(id) {
+  try {
+    const { data } = await axios.get(`${BY_ID}${507086}?api_key=${KEY}`);
+    return data;
+  } catch (error) {
+    console.error('ERROR');
   }
-
+}
 
 // Проверка работаспособности рендера
 // fetchByID().then(data => {
@@ -50,7 +52,7 @@ fetchTrendMovies().then(data => {
   renderTrendCollection(data);
   console.log('byTrends', data);
 });
-// // Проверка пагинации 
+// // Проверка пагинации
 // function more() {
 //   page += 1;
 //   fetchTrendMovies().then(data => {
