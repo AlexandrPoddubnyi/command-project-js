c600ff8a696d0f96612569be1421ba34caf788d6
Dante4Rungun <denischuprin9619@gmail.com>
2022-07-26 14:42:43 +0300

    fix

--- src/js/personal_cabinet/cabinet-open.js
+++ src/js/personal_cabinet/cabinet-open.js
@@ -3,12 +3,23 @@
     modal: document.querySelector("[cabinet-modal]"),
   };
 
-  document.querySelector('.header__top').addEventListener('click', (event) => {
-    if (document.querySelector("[cabinet-modal-open]") != null) {
-      document.querySelector("[cabinet-modal-open]").addEventListener("click", toggleModal);
-      document.querySelector("[cabinet-modal-close]").addEventListener("click", toggleModal);    
-    }  
-  })
+  if (document.querySelector('.header__top') != null) {
+    document.querySelector('.header__top').addEventListener('click', (event) => {
+      if (document.querySelector("[cabinet-modal-open]") != null) {
+        document.querySelector("[cabinet-modal-open]").addEventListener("click", toggleModal);
+        document.querySelector("[cabinet-modal-close]").addEventListener("click", toggleModal);    
+      }  
+    })
+  }
+
+  if (document.querySelector('.navigation') != null) {
+    document.querySelector('.navigation').addEventListener('click', (event) => {
+      if (document.querySelector("[cabinet-modal-open]") != null) {
+        document.querySelector("[cabinet-modal-open]").addEventListener("click", toggleModal);
+        document.querySelector("[cabinet-modal-close]").addEventListener("click", toggleModal);    
+      }  
+    })
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
